a8a86abfbbbcb406921778fa689cbca10dccd74a
J.Ebbighausen <j.ebbighausen@mfn.berlin>
2020-12-03 18:02:24 +0100

    inti

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,15 @@
+package controllers
+
+import play.api.mvc._
+
+import javax.inject._
+
+@Singleton
+class HomeController @Inject() (val controllerComponents: ControllerComponents)
+    extends BaseController {
+
+  def index() =
+    Action {
+      Ok
+    }
+}
